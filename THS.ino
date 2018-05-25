adc95befc580600053277deb613c877b36d6fc3b
Vasil <vp@clevertec.ru>
2018-05-25 22:13:54 +0300

    wokring with BMP280

--- THS.ino
+++ THS.ino
@@ -1,39 +1,90 @@
+
+
 #include <LiquidCrystal.h>
-#define DEBUG
+#include <SoftwareSerial.h>
+#include <Adafruit_BMP280.h>
+#include <Adafruit_BME280.h>
 #include "TempDS.h"
 #include "mh_z19.h"
 
-
-
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 9, en = 8, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(9, 8, 7, 6, 5, 4);//rs, en, d4, d5, d6, d7
+
+unsigned int minPPM = 5000;
+unsigned int maxPPM = 0;
+
+SoftwareSerial btSerial(10,11);
+
+Adafruit_BMP280 bme;
 
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
-  lcd.setCursor(0, 1);
+
+  btSerial.begin(9600);
 
   Serial.begin(9600);
   mh_setup();
 
   pinMode(LED_BUILTIN, OUTPUT);
-  //pinMode(en,OUTPUT);
+
+  /*bme.settings.commInterface = I2C_MODE;
+  bme.settings.I2CAddress = 0x76; //Адрес датчика, в моём случае не стандартный
+  bme.settings.runMode=3;
+  bme.settings.tStandby=5;
+  bme.settings.filter=0;
+  bme.settings.tempOverSample = 1;
+  bme.settings.humidOverSample = 1;*/
+  if (!bme.begin(0x76)) {
+    lcd.print(F("Error initializing BMP"));
+  }
 }
 
 void loop() {
   
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
+  delay(10);
   digitalWrite(LED_BUILTIN, LOW);
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0,0);
   lcd.print(getTemperature());
-  lcd.setCursor(8,1);
-  lcd.print(mh_getPPM());
+  lcd.print(F("/"));
+  lcd.print(bme.readTemperature());
+  lcd.print(F("\xDF"));lcd.print(F("C  "));
+  lcd.setCursor(0,1);
+  unsigned int ppm = mh_getPPM();
+  unsigned int sec = millis()/1000;
+  
+  lcd.print(ppm);
+  if (sec < 1*60) {
+    lcd.print(F("?  "));
+  } else {
+    if (ppm > maxPPM) maxPPM=ppm;
+    if (ppm < minPPM) minPPM=ppm;
+    lcd.print(F(" ("));
+    lcd.print(minPPM);
+    lcd.print(F("-"));
+    lcd.print(maxPPM);
+    lcd.print(F(")  "));
+  }
+  
+  btSerial.print(sec);btSerial.print(F("\t"));btSerial.println(ppm);
+  btSerial.listen();
+    
+  delay(10000);
+  if (btSerial.available()> 0)
+  {
+    byte incomingByte = btSerial.read(); // считываем байт
+    if(incomingByte == '0') {
+      minPPM = 5000;
+      btSerial.println(F("min reset"));
+    }
+    if(incomingByte == '1') {
+      maxPPM = 0;
+      btSerial.println(F("max reset"));
+    }
+  }
   
 }
 
--- mh_z19.cpp
+++ mh_z19.cpp
@@ -1,20 +1,41 @@
 #include <SoftwareSerial.h>
 #include <Arduino.h>
 #include "mh_z19.h"
-SoftwareSerial mySerial(A0, A1); // A0 - к TX сенсора, A1 - к RX
+#define DEBUG
+SoftwareSerial mhSerial(A0, A1); // A0 - к TX сенсора, A1 - к RX
 
 byte cmd[9] = {0xFF,0x01,0x86,0x00,0x00,0x00,0x00,0x00,0x79}; 
 unsigned char response[9];
 
 void mh_setup() {
-  mySerial.begin(9600);
+  mhSerial.begin(9600);
+  byte icmd[9] = {0xFF, 0x01, 0x99, 0x00, 0x00, 0x00, 0x13, 0x88, 0xCB};
+
+  mhSerial.write(cmd, 9);
+  memset(response, 0, 9);
+  mhSerial.readBytes(response, 9);  
+  #ifdef DEBUG
+  int i;
+  byte crc = 0;
+  for (i = 1; i < 8; i++) crc+=response[i];
+  crc = 255 - crc;
+  crc++;
+  if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
+    Serial.println("MH: setRange CRC error: " + String(crc) + " / "+ String(response[0]) + "," + String(response[1]) + ",...," +String(response[8]));
+  } else
+  {
+    Serial.println("MH: setRange OK");
+  }
+  #endif
+    
 }
 
 unsigned int mh_getPPM() 
 {
-  mySerial.write(cmd, 9);
+  mhSerial.listen();
+  mhSerial.write(cmd, 9);
   memset(response, 0, 9);
-  mySerial.readBytes(response, 9);
+  mhSerial.readBytes(response, 9);
   int i;
   byte crc = 0;
   for (i = 1; i < 8; i++) crc+=response[i];
@@ -22,17 +43,17 @@ unsigned int mh_getPPM()
   crc++;
 
   if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
-    //#ifdef DEBUG
-    Serial.println("MH: CRC error: " + String(crc) + " / "+ String(response[8]));
-    //#endif
-    return 0;
+    #ifdef DEBUG
+    Serial.println("MH: CRC error: " + String(crc) + " / "+ String(response[0]) + "," + String(response[1]) + ",...," +String(response[8]));
+    #endif
+    return 1;
   } else {
     unsigned int responseHigh = (unsigned int) response[2];
     unsigned int responseLow = (unsigned int) response[3];
     unsigned int ppm = (256*responseHigh) + responseLow;
-    //#ifdef DEBUG
+    #ifdef DEBUG
     Serial.print(F("MH: "));Serial.println(ppm);
-    //#endif
+    #endif
     return ppm;
   }
 }
